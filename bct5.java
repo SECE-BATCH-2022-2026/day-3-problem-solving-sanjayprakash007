3abef8341bd7daf3bf7ad6aba68c97e7d5f9da20
sanjayprakash007 <127585877+sanjayprakash007@users.noreply.github.com>
2025-12-08 15:36:10 +0530

    Add files via upload

---
+++ bct5.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String[] inp = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            inp[i] = br.readLine();
+        }
+
+        String[] op = UserMainCode.removeDuplicates(inp);
+
+        System.out.println("Using Lambda");
+        Arrays.stream(op).forEach(System.out::println);
+
+        br.close();
+    }
+}
+
+class UserMainCode {
+    public static String[] removeDuplicates(String[] inp) {
+        Set<String> ts = new TreeSet<>();
+
+        for (String s : inp) {
+            ts.add(s);
+        }
+
+        String[] res = new String[ts.size()];
+        int i = 0;
+
+        for (String s : ts) {
+            res[i++] = s;
+        }
+        return res;
+    }
+}
